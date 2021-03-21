e4ba2141e2382d69513452b37bed7c330f636142
Rodrigo Barrera <36526580+Cracop@users.noreply.github.com>
2021-03-21 14:04:01 -0600

    Ya se crean las piezas y se utiliza la notacion FEN para darle un estado al tablero

--- ajedrez.py
+++ ajedrez.py
@@ -10,10 +10,12 @@ Torre = 5
 Los valores de las piezas serán positivos para mí y negativos para mi oponente
 Puedo crear una clase que sea la pieza, la cual va a contener su valor, posición, movidas posibles
 Checar la notación FEN para generar las posiciones
+Notación FEN para el inicio rnbqkbnr/pppppppp/8/8/8/8/PPPPPPPP/RNBQKBNR
+
 El tablero va a ser una matriz, puedo usar modulo y división para encontrar la coordenada
-Uso un diccionario para guardar la información de las piezas, tengo una clase por tipo de pieza 
+Uso un diccionario para guardar la información de las piezas, tengo una clase por tipo de pieza
 (o un atributo que diga el tipo, por definir).
-Un diccionario para las piezas blancas y un diccionario para las piezas negras. 
+Un diccionario para las piezas blancas y un diccionario para las piezas negras.
 La llave es la coordenada, el valor es el objeto de la pieza
 
 Movidas posibles: Esto es para moverse un cuadrito en esas direcciones
@@ -26,13 +28,23 @@ Para más de un cuadrito lo multiplico por 2 y ya
     Mover diagonalmente arriba a la izquierda le resto 9
     Mover diagonalmente abajo a la derecha le sumo 9
     Mover diagonalmente abajo a la izquierda le sumo 7
-    +Ver como demonios mover al caballo 
+    +Ver como demonios mover al caballo
       19  21     le resto 17 o 15
     26      30   le resto 10 o 6
-        36          
+        36
     42      46   le sumo 6 o 10
       51  53     le sumo 15 o 17
 
+tipos:
+    P - Peon
+    C - Caballo
+    A - Alfil
+    T - Torre
+    D - Dama
+    R - Rey
+
+    XN - Pieza negra
+    XB - pieza blanca
 """
 
 
@@ -42,16 +54,26 @@ class Tablero:
 
 
 class Pieza:
-    def __init__(self, valor, posicion, movidasPosible):
+    def __init__(self, valor, posicion, tipo):
         self.valor = valor
+        self.posicion = posicion
+        self.tipo = tipo
+        self.movidas = []
 
 
 def imprimirMatriz(matriz):
     tabla = ""
     num = 0
     for i in range(len(matriz)):
+        tabla += "|"
         for j in range(len(matriz[i])):
-            tabla += str(matriz[i][j]+num)+" "
+            if (matriz[i][j] == 0):
+                #tabla += str(matriz[i][j]+num)+" "
+                tabla += ".."
+                tabla += "|"
+            else:
+                tabla += matriz[i][j].tipo
+                tabla += "|"
             num += 1
         tabla += "\n"
     print(tabla)
@@ -59,6 +81,75 @@ def imprimirMatriz(matriz):
 
 # La casilla se da por un número que va de 0 a 63
 # Fila = num/8; Columna = num % 8
+"""Rey = No tiene valor, pues no se puede capturar
+Reina = 9
+Alfil = 3.25 (Según Bobby Fischer y Kasparov)
+Caballo = 3
+Peón = 1
+Torre = 5"""
+
+
+def crearPiezas(matriz, blancas, negras):
+    # Notacion FEN, con esto puedo dar un estado del tablero
+    FEN = "rnbqkbnr/pppppppp/8/8/8/8/PPPPPPPP/RNBQKBNR"
+    FEN = FEN.replace('/', '')
+    # Va de abajo hacia arriba, minusculas son negras, mayusculas son blancas
+    num = 0
+    for i in range(len(FEN)):
+        if FEN[i].isnumeric():
+            num += int(FEN[i])
+            num -= 1
+        if FEN[i] == "r":  # Torre negra
+            torre = Pieza(5, num, "Tn")
+            matriz[int(num/8)][num % 8] = torre
+            negras[num] = torre
+        elif FEN[i] == "n":  # caballo negro
+            caballo = Pieza(3, num, "Cn")
+            matriz[int(num/8)][num % 8] = caballo
+            negras[num] = caballo
+        elif FEN[i] == "b":  # alfil negro
+            alfil = Pieza(3.25, num, "An")
+            matriz[int(num/8)][num % 8] = alfil
+            negras[num] = alfil
+        elif FEN[i] == "q":  # dama negra
+            dama = Pieza(9, num, "Dn")
+            matriz[int(num/8)][num % 8] = dama
+            negras[num] = dama
+        elif FEN[i] == "k":  # rey negro
+            rey = Pieza(None, num, "Rn")
+            matriz[int(num/8)][num % 8] = rey
+            negras[num] = rey
+        elif FEN[i] == "p":  # peon negro
+            peon = Pieza(1, num, "Pn")
+            matriz[int(num/8)][num % 8] = peon
+            negras[num] = peon
+        elif FEN[i] == "P":  # peon blanco
+            peon = Pieza(1, num, "Pb")
+            matriz[int(num/8)][num % 8] = peon
+            blancas[num] = peon
+        if FEN[i] == "R":  # Torre negra
+            torre = Pieza(5, num, "Tb")
+            matriz[int(num/8)][num % 8] = torre
+            blancas[num] = torre
+        elif FEN[i] == "N":  # caballo negro
+            caballo = Pieza(3, num, "Cb")
+            matriz[int(num/8)][num % 8] = caballo
+            blancas[num] = caballo
+        elif FEN[i] == "B":  # alfil negro
+            alfil = Pieza(3.25, num, "Ab")
+            matriz[int(num/8)][num % 8] = alfil
+            blancas[num] = alfil
+        elif FEN[i] == "Q":  # dama negra
+            dama = Pieza(9, num, "Db")
+            matriz[int(num/8)][num % 8] = dama
+            blancas[num] = dama
+        elif FEN[i] == "K":  # rey negro
+            rey = Pieza(None, num, "Rb")
+            matriz[int(num/8)][num % 8] = rey
+            blancas[num] = rey
+        num += 1
+
+
 tablero = [[0, 0, 0, 0, 0, 0, 0, 0],
            [0, 0, 0, 0, 0, 0, 0, 0],
            [0, 0, 0, 0, 0, 0, 0, 0],
@@ -69,4 +160,8 @@ tablero = [[0, 0, 0, 0, 0, 0, 0, 0],
            [0, 0, 0, 0, 0, 0, 0, 0]
            ]
 
+piezasBlancas = {}
+piezasNegras = {}
+crearPiezas(tablero, piezasBlancas, piezasNegras)
 imprimirMatriz(tablero)
+# print(piezasNegras)
