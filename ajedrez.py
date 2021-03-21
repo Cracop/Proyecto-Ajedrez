d683a306ce428c61209f5c9b72eb449b77750a1b
Rodrigo Barrera <36526580+Cracop@users.noreply.github.com>
2021-03-20 21:44:29 -0600

    Agregado lo que necesito para los movimientos diagonales y del caballo

--- ajedrez.py
+++ ajedrez.py
@@ -16,20 +16,24 @@ Uso un diccionario para guardar la información de las piezas, tengo una clase p
 Un diccionario para las piezas blancas y un diccionario para las piezas negras. 
 La llave es la coordenada, el valor es el objeto de la pieza
 
-Movidas posibles:
+Movidas posibles: Esto es para moverse un cuadrito en esas direcciones
+Para más de un cuadrito lo multiplico por 2 y ya
     Mover hacia abajo le sumo 8
     Mover hacia arriba le resto 8
     Mover hacia la izquierda le resto 1
     Mover hacia la derecha le sumo 1
-    Mover diagonalmente arriba a la derecha
-    Mover diagonalmente arriba a la izquierda
-    Mover diagonalmente abajo a la derecha
-    Mover diagonalmente abajo a la izquierda
-    Ver como demonios mover al caballo
+    Mover diagonalmente arriba a la derecha le resto 7
+    Mover diagonalmente arriba a la izquierda le resto 9
+    Mover diagonalmente abajo a la derecha le sumo 9
+    Mover diagonalmente abajo a la izquierda le sumo 7
+    +Ver como demonios mover al caballo 
+      19  21     le resto 17 o 15
+    26      30   le resto 10 o 6
+        36          
+    42      46   le sumo 6 o 10
+      51  53     le sumo 15 o 17
 
 """
-
-# Creo una clase que se llame tablero
 
 
 class Tablero:
@@ -44,9 +48,11 @@ class Pieza:
 
 def imprimirMatriz(matriz):
     tabla = ""
+    num = 0
     for i in range(len(matriz)):
         for j in range(len(matriz[i])):
-            tabla += str(matriz[i][j])+" "
+            tabla += str(matriz[i][j]+num)+" "
+            num += 1
         tabla += "\n"
     print(tabla)
 
