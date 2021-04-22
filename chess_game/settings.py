fd767167f6beeaf0b3c6010a85f0c9bee08a3a31
Bruno Vitte <bruno.vitte@itam.mx>
2021-04-21 23:06:52 -0500

    tratando de subir

--- chess_game/settings.py
+++ chess_game/settings.py
@@ -127,6 +127,8 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
 
 STATIC_URL = '/static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'chess_board', 'build')
+
 
 STATICFILES_DIRS = (
     os.path.join(BASE_DIR, 'chess_board', "build",
